287fafd82c8fb9389be66ac6bd0286ce9579d0e1
Lesther Villatoro <rodnyvillatorotzoc@gmail.com>
2023-02-14 09:46:10 -0600

    Se modificó main.py y se agregó gitignore
    Se utilizó un archivo py para agregar una carpeta para luego ignorarla con gitignore

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Lesther Rodny Hámilton Villatoro Tzoc 1583623")
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
